aeb35aafd03c2a4c415c1eb27915550ef28e6f21
masum-2020 <almsm014@gmail.com>
2022-05-02 23:43:57 +0600

    my health condition app

---
+++ index.js
@@ -0,0 +1,172 @@
+// while loop
+
+// let num = 1;
+
+// while(num < 10){
+//     console.log('This is while loop', num);
+//     num++;  
+// }
+// document.write("<h1>Your loop works fine</h1>");
+
+// loop
+
+// const cars = ['Bus', 'car', 'Bike', 'Many Bus']
+
+// for (let i = 0; i < cars.length; i++) {
+//     console.log(cars[i]);
+// }
+
+// for (car of cars) 
+//    console.log(car);
+
+// for(let i = 0; i <= 15; i++ ){
+//     console.log('Hello World', i);
+//     if(i === 14) break;
+// }
+
+
+// switch
+// let x = 1;
+
+// switch (x){
+//     case 0:
+//         x = "off"
+//         break;
+//      case 1:
+//         x = "on"
+// }
+
+// console.log(x);
+
+
+
+// let day = 5;                                                                                                        
+
+// switch (day){
+//     case 0:
+//         day = "Sunday. Its office day.";
+//     break;
+//     case 1:
+//         day = "Monday. Its office day.";
+//     break;
+//     case 2:
+//         day = "Tuesday. Its office day.";
+//     break;
+//     case 3:
+//         day = "Wednesday. Its office day.";
+//     break;
+//     case 4:
+//         day = "Thursday. Its office day.";
+//     break;
+//     case 5:
+//         day = "Friday. Its weekend.";
+//     break;
+//     case 6:
+//         day = "Saturday. Its weekend.";
+//     break;
+//     default:
+//         day = "Not Found";
+// }
+//    console.log(day);
+
+
+// condition.
+// 6am to 12am "Good Morning"
+// 12:01pm to 6pm "Good Afternoon"
+// Otherwise "Good Evening"
+// let time = new Date().getHours();
+
+// let time = 6;
+
+// if(time >= 6 && time < 12){
+//     console.log('Good Morning');
+// }
+// else if(time >= 12 && time < 18){
+//     console.log('Good Afternoon');
+// }
+// else{
+//     console.log('Good Evening');
+// }
+
+// let numOne = 'Ali';
+
+// console.log(Boolean(numOne));
+
+// let upperValue = 6;
+// let lowerValue = 0;
+
+// let resultValue = Math.floor(Math.random() * (upperValue - lowerValue) +1);
+
+// console.log(resultValue);
+
+
+// console.log(Math.round(3.5));
+// console.log(Math.pow(3,5));
+// console.log(Math.sqrt(144));
+// console.log(Math.abs(3.5));
+// console.log(Math.ceil(3.5));
+// console.log(Math.floor(3.5));
+// console.log(Math.sin(90*Math.PI/180));
+// console.log(Math.cos(0*Math.PI/180));
+// console.log(Math.min(0, 5, 8, 55, 88));
+// console.log(Math.max(0, 5, 8, 55, 88));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()));
+// console.log(Math.random().toFixed(4) * 10 +1 );
+
+
+// function hello(){
+//     document.write('Hello World')
+// }
+// function hello1(){
+//     document.write('I want to be a world best web developer');
+// }
+
+
+
+
+// function name (firstName, lastName){
+//     console.log('Hello ' + firstName + '' + lastName);
+//     console.log('Hello ' + firstName);
+// }
+
+// name ('Masum ', ' Billah');
+
+// function math(number){
+//     return number + number;
+// }
+
+// console.log(math(8));
+
+// let hiMorning = function fretting(){
+
+// }
+
+
+
+
+// function name(){
+//     console.log('Hello World')
+// }
+// name();
+
+// let phone = 'OnePlus'
+// let model = 7;
+
+// let phone = ['Samsung', ' Apple', ' Bodaphone'];
+// phone[3] = ' Nokia';
+
+// let phone = {
+//     model: 'iphone 12',
+//     storage: '128GB',
+//     color: 'Golden',
+// };
+
+// phone.storage = '256GB';
+
+
+//  console.log(phone);
+
+
+// document.getElementById("html").innerHTML= phone.model;
+
